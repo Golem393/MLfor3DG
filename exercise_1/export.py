9aaf5c9c7a28f6e97716698f9302437209892474
Flavio Arrigoni <f.arrigoni@student.tugraz.at>
2024-11-06 18:27:52 +0100

    1.4 and 1.5

--- exercise_1/export.py
+++ exercise_1/export.py
@@ -9,13 +9,13 @@ def export_mesh_to_obj(path, vertices, faces):
     :param faces: Mx3 faces
     :return: None
     """
-
+    with open(path, "w") as file:
+        for v in vertices:
+            file.write(f"v {v[0]:.1f} {v[1]:.1f} {v[2]:.1f}\n")
+        for f in faces:
+            file.write(f"f {f[0]} {f[1]} {f[2]}\n")
     # write vertices starting with "v "
     # write faces starting with "f "
-
-    # ###############
-    # TODO: Implement
-    raise NotImplementedError
     # ###############
 
 
@@ -26,8 +26,7 @@ def export_pointcloud_to_obj(path, pointcloud):
     :param pointcloud: Nx3 points
     :return: None
     """
-
-    # ###############
-    # TODO: Implement
-    raise NotImplementedError
+    with open(path, "w") as file:
+        for p in pointcloud:
+            file.write(f"v {p[0]:.1f} {p[1]:.1f} {p[2]:.1f}\n")
     # ###############
--- exercise_1/point_cloud.py
+++ exercise_1/point_cloud.py
@@ -9,9 +9,21 @@ def sample_point_cloud(vertices, faces, n_points):
     :param faces: Mx3 numpy array of mesh faces
     :param n_points: number of points to be sampled
     :return: sampled points, a numpy array of shape (n_points, 3)
-    """
 
-    # ###############
-    # TODO: Implement
-    raise NotImplementedError
+    """
+    n_points_per_face = n_points // len(faces)
+    remaining_points = n_points % len(faces)
+    points = np.empty((0, 3))
+    for idx, face in enumerate(faces):
+        n_per_face = n_points_per_face
+        if idx < remaining_points:
+            n_per_face += 1
+        for _ in range(n_per_face):
+            r_1 = np.random.rand(0,1)
+            r_2 = np.random.rand(0,1)
+            u = 1 - np.sqrt(r_1)
+            v = np.sqrt(r_1) * (1 - r_2)
+            w  = np.sqrt(r_1) * r_2
+            new_point = u * vertices[face[0]] + v * vertices[face[1]] + w * vertices[face[2]]
+            points = np.vstack([points, new_point])
     # ###############
